90c1b8c9c8744c5aac1a475b7abf00264846b3a7
wonseok <dnjstjr245@gmail.com>
2023-02-17 16:35:14 +0900

    [chore] buildSrc + Kotlin DSL을 통한 의존성 관리 설정 (#1)

---
+++ buildSrc/src/main/java/Constants.kt
@@ -0,0 +1,8 @@
+object Constants {
+    const val packageName = "com.freetreechair.preat"
+    const val compileSdk = 33
+    const val minSdk = 24
+    const val targetSdk = 33
+    var versionCode = 1
+    var versionName = "1.0.0"
+}
