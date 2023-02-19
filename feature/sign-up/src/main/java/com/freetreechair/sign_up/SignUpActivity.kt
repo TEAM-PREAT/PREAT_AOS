f6eed851f4c685fa8104782ff4bf807bd12e7273
wonseok <dnjstjr245@gmail.com>
2023-02-19 22:46:02 +0900

    [chore] sign-up 모듈 추가 및 gradle 설정 (#6)

---
+++ feature/sign-up/src/main/java/com/freetreechair/sign_up/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.freetreechair.sign_up
+
+import android.os.Bundle
+import com.freetreechair.common.base.BindingActivity
+import com.freetreechair.sign_up.databinding.ActivitySignUpBinding
+
+class SignUpActivity : BindingActivity<ActivitySignUpBinding>(R.layout.activity_sign_up) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
---
+++ feature/sign-up/src/main/java/com/freetreechair/sign_up/nickname/NicknameFragment.kt
@@ -0,0 +1,14 @@
+package com.freetreechair.sign_up.nickname
+
+import android.os.Bundle
+import android.view.View
+import com.freetreechair.common.base.BindingFragment
+import com.freetreechair.sign_up.R
+import com.freetreechair.sign_up.databinding.FragmentNicknameBinding
+
+class NicknameFragment : BindingFragment<FragmentNicknameBinding>(R.layout.fragment_nickname) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
