7c5ab15abc0ab227bd362a3026f2ff396b21385e
wonseok <dnjstjr245@gmail.com>
2023-02-21 13:50:48 +0900

    [feat] 닉네임 완료 카드뷰 애니메이션 적용 (#6)

--- feature/sign-up/src/main/java/com/freetreechair/sign_up/welcome/WelcomeFragment.kt
+++ feature/sign-up/src/main/java/com/freetreechair/sign_up/welcome/WelcomeFragment.kt
@@ -1,7 +1,10 @@
 package com.freetreechair.sign_up.welcome
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.view.View
+import android.view.animation.LinearInterpolator
 import androidx.navigation.fragment.navArgs
 import com.freetreechair.common.base.BindingFragment
 import com.freetreechair.sign_up.R
@@ -12,5 +15,21 @@ class WelcomeFragment : BindingFragment<FragmentWelcomeBinding>(R.layout.fragmen
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.tvNickname.text = args.nickname
+        setAnimationOnCard()
+    }
+
+    private fun setAnimationOnCard() {
+        ObjectAnimator.ofFloat(binding.clWelcome, "translationY", TRANSLATION_VALUE).apply {
+            interpolator = LinearInterpolator()
+            repeatMode = ValueAnimator.REVERSE
+            repeatCount = ValueAnimator.INFINITE
+            duration = DURATION_TIME
+            start()
+        }
+    }
+
+    companion object {
+        const val DURATION_TIME = 3000L
+        const val TRANSLATION_VALUE = -50F
     }
 }
