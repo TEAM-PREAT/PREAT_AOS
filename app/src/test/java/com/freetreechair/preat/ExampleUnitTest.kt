f9f4eec89d891561c5086ab13a490a6c24f85061
wonseok <dnjstjr245@gmail.com>
2023-02-23 11:05:11 +0900

    [refactor] wildcard import 수정 (#9)

--- app/src/test/java/com/freetreechair/preat/ExampleUnitTest.kt
+++ app/src/test/java/com/freetreechair/preat/ExampleUnitTest.kt
@@ -2,7 +2,7 @@ package com.freetreechair.preat
 
 import org.junit.Test
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 
 /**
  * Example local unit test, which will execute on the development machine (host).
