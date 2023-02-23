94bb99b014d3b9f4cea7d3770dc8e60746d2763c
wonseok <dnjstjr245@gmail.com>
2023-02-24 02:22:26 +0900

    [feat] repository binds 메서드 추가 (#12)

--- app/src/main/java/com/freetreechair/preat/di/RepositoryModule.kt
+++ app/src/main/java/com/freetreechair/preat/di/RepositoryModule.kt
@@ -3,9 +3,11 @@ package com.freetreechair.preat.di
 import com.freetreechair.data.disgust.repository.DisgustRepositoryImpl
 import com.freetreechair.data.login.repository.LoginRepositoryImpl
 import com.freetreechair.data.nickname.repository.NicknameRepositoryImpl
+import com.freetreechair.data.taste.repository.TasteRepositoryImpl
 import com.freetreechair.domain.disgust.repository.DisgustRepository
 import com.freetreechair.domain.login.repository.LoginRepository
 import com.freetreechair.domain.nickname.repository.NicknameRepository
+import com.freetreechair.domain.taste.repository.TasteRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -18,10 +20,16 @@ interface RepositoryModule {
     @Binds
     @Singleton
     fun bindsLoginRepository(repository: LoginRepositoryImpl): LoginRepository
+
     @Binds
     @Singleton
     fun bindsNicknameRepository(repository: NicknameRepositoryImpl): NicknameRepository
+
     @Binds
     @Singleton
     fun bindsDisgustRepository(repository: DisgustRepositoryImpl): DisgustRepository
+
+    @Binds
+    @Singleton
+    fun bindsTasteRepository(repository: TasteRepositoryImpl): TasteRepository
 }
