ab22b21ba382525bad16d380d9e7358169f645fb
wonseok <dnjstjr245@gmail.com>
2023-02-24 02:15:53 +0900

    [feat] 식당 평가 ui 클래스 초기화 (#12)

---
+++ feature/sign-up/src/main/java/com/freetreechair/sign_up/evaluate/EvaluateFragment.kt
@@ -0,0 +1,13 @@
+package com.freetreechair.sign_up.evaluate
+
+import android.os.Bundle
+import android.view.View
+import com.freetreechair.common.base.BindingFragment
+import com.freetreechair.sign_up.R
+import com.freetreechair.sign_up.databinding.FragmentEvaluateBinding
+
+class EvaluateFragment : BindingFragment<FragmentEvaluateBinding>(R.layout.fragment_evaluate) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
